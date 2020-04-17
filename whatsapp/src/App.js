19feba40defaea0f8203a86b2e66982200f8c7ec
Tais Itoo <tais2001@gmail.com>
2020-04-17 14:58:01 -0300

    whats

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Conteudo=styled.div ` 
+  width:50%; 
+  border: 1px solid black;`
+ 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="container" >
+  <Conteudo>Oi</Conteudo>
+  
+</div>
   );
 }
+
 
 export default App;
